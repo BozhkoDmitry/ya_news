e787390597d4de6eb23825124422c399f4f01b44
Dmitry Bozhko <dmitry.bozhko.inbox@yandex.ru>
2024-05-27 22:24:14 +0700

    -

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -14,15 +14,3 @@ def test_detail_page(db, client, news):
     url = reverse('news:detail', args=(news.id,))
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
-
-
-@pytest.mark.parametrize(
-        'page, key',
-        (
-            ('news:home', None),
-            ('news:detail', pytest.lazy_fixture('news')),
-            ('users:login', None),
-            ('users:logout', None),
-            ('users:signup', None),
-        )
-)
